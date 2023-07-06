80bf21ccc0bcf27d9ed91ddf50598b307f7dd892
hansolo-N <91372832+hansolo-N@users.noreply.github.com>
2023-07-06 15:41:08 +0200

    loading users from GH component

---
+++ src/components/users/UserResults.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { useEffect,useState } from 'react'
+
+
+function UserResults() {
+    const [users,setUsers] = useState([])
+    const [loading,setLoading] = useState(true)
+
+
+    useEffect(()=>{
+        fetchUsers()
+    },[])
+
+
+    const fetchUsers = async ()=>{
+        const response = await fetch(`${process.env.REACT_APP_github_url}/users`,
+        {
+            // headers:{
+            //     Authorization: `token ${process.env.REACT_APP_github_token }`,
+            // }
+        })
+
+        const data = await response.json();
+        setUsers(data)
+        setLoading(false)
+    }
+    if(!loading){
+        return (
+            <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
+                {users.map((user)=>(
+                    <h3>{user.login}</h3>
+                ))}
+            </div>
+          )
+    }
+    else{
+        return(
+            <h3>loading......</h3>
+        )
+    }
+
+}
+
+export default UserResults
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import UserResults from '../components/users/UserResults'
 
 function Home() {
   return (
-    <h1 className="text-6xl">Welcome</h1>
+    <div>
+      <UserResults/>
+    </div>
+
   )
 }
 
